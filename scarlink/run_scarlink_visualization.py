ae801fe1598507ca1b69853cb937b09dd2d8f588
Sneha Mitra <sneha@cs.duke.edu>
2023-10-24 13:12:05 -0400

    added help for scarlink_plot

--- scarlink/run_scarlink_visualization.py
+++ scarlink/run_scarlink_visualization.py
@@ -5,9 +5,9 @@ import scarlink.src.visualization as scv
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--outdir', required=True, type=str)
-    parser.add_argument('--genes', required=True, type=str)
-    parser.add_argument('-c', '--celltype', required=True, type=str)
+    parser.add_argument('-o', '--outdir', required=True, type=str, help="Output directory. Must be the same as the output directory used for scarlink_processing and scarlink")
+    parser.add_argument('--genes', required=True, type=str, help="Genes to plot the output for. scarlink needs to be run before this on the given genes.")
+    parser.add_argument('-c', '--celltype', required=True, type=str, help="Cell type column name. scarlink needs to be run before-hand with the same celltype.")
     args = parser.parse_args()
     
     # check if genes are in file
